8233588c53305e61b81f5fc39238c906ee5d624d
MrNasaMan <109177113+MrNasaMan@users.noreply.github.com>
2022-12-04 21:28:03 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -4,15 +4,24 @@ const url = 'https://laartcc.org/topten';
 const Discord = require('discord.js');
 const axios = require('axios');
 const { MessageEmbed } = require('discord.js');
+const terminalProcedures = require('terminal-procedures')
+var ifrText = [];
 var toptenText = [];
+var eventText = [];
+const url2 = 'https://laartcc.org';
+const vatsim_stats = require('vatsim-stats');
+
+
+
+
 
 
 
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] }); 
 
-const prefix = '-';
+const prefix = '!';
 
-const token = 'OTk0NjYzNzA1ODM5MTQ5MDc2.GQ_73d.62jbgWt7aULnds1ukNEayCaON3DlkNzAKlpZ5g';
+const token = '';
 
 
 client.once('ready', () => {
@@ -30,155 +39,176 @@ client.on('message', async message =>{
   const args = message.content.slice(prefix.length).split(/ +/);
   const command = args.shift().toLowerCase();
 
-  const firstPart = command.slice(1, 4);
-  const secondPart = command.slice(4, 7);
-  const metarFirst = command.slice(0,4);
-  const metarSecond = command.slice(4, 9);
-  const infoFirst = command.slice(0,4);
-  const infoSecond = command.slice(4, 8);
+
   const tecFirst = command.slice(0,3);
   const tecSecond = command.slice(3, 6);
   const tecThird = command.slice(6, 9);
   const statsSecond = command.slice(5, 13);
   const statsfirst = command.slice(0, 5);
   const topten = command.slice(0, 8);
+  const online1 = command.slice(0, 6);
+  const analyzer1 = command.slice(8, 16);
+  const analyzer2 = command.slice(0, 4);
+  const analyzer3 = command.slice(4, 8);
+  const traffic1 = command.slice(0,7);
+  const traffic2 = command.slice(7,11);
  
 
-  if (secondPart === 'apd'){
-    const secondEmbed = new MessageEmbed()
-	  .setColor('#0099ff')
-	  .setTitle('K'+ firstPart.toUpperCase() + ' Airport Diagram')
-    .setURL('https://flightaware.com/resources/airport/' + firstPart.toUpperCase() + '/APD/AIRPORT+DIAGRAM/pdf')
-    .setDescription('K' + firstPart.toUpperCase() + ' Airport Diagram Can Be Found Here')
-    .setImage('https://flightaware.com/resources/airport/' + firstPart.toUpperCase() + '/APD/AIRPORT+DIAGRAM/png')
-	  message.channel.send({ embeds: [secondEmbed] });
-  }
-  if (command == 'checkroute'){
-    message.reply('https://flightaware.com/statistics/ifr-route/')
+  
+ 
+
+  if (analyzer1 === 'ifrroute'){
+    const url3 = 'https://flightaware.com/analysis/route.rvt?origin=' + analyzer2.toUpperCase() + '&destination=' + analyzer3.toUpperCase()
+    puppeteer
+    .launch()
+    .then(browser => browser.newPage())
+    .then(page => {
+    return page.goto(url3).then(function() {
+      return page.content();
+    });
+  })
+  .then(html => {
+    const $ = cheerio.load(html);
+    ifrText = []
+    $('#mainBody > div.pageContainer > table > tbody > tr:nth-child(4) > td > table > tbody > tr:nth-child(1) > td:nth-child(5) > a').each(function() {
+      ifrText.push(
+        $(this).text(),
+      );
+    });
+    $('#mainBody > div.pageContainer > table > tbody > tr:nth-child(4) > td > table > tbody > tr:nth-child(2) > td:nth-child(5) > a').each(function() {
+        ifrText.push(
+          $(this).text(),
+        );
+    });
+      
+    $('#mainBody > div.pageContainer > table > tbody > tr:nth-child(4) > td > table > tbody > tr:nth-child(3) > td:nth-child(5) > a').each(function() {
+        ifrText.push(
+          $(this).text(),
+        );
+    });
+    
+    var text2=[];
+      ifrText.forEach((item, index) => {
+        text2 += index + ": " + item + "\n"; 
+      });
+      const analyzerEmbed = new MessageEmbed()
+      .setColor('0x5f5e66')
+      .setTitle(analyzer2.toUpperCase() + ' to ' + analyzer3.toUpperCase() + ' IFR routes in terms of popularity')
+      .setDescription(text2.toString())
+      .setFooter({ text: 'Not for real world use! Bot coded by DY and BY. Routes provided by FlightAware', iconURL: 'https://cdn.discordapp.com/icons/612373312051478662/7fad3012e32dd58264ea884473a2552e.webp?size=96' });
+      if (text2[0] == '0') {
+        message.channel.send({ embeds: [analyzerEmbed] });
+      }else{
+        const error4Embed = new MessageEmbed()
+        .setColor('0xff0000')
+        .setTitle('**Error!**')
+        .setDescription('No Route Found.')
+        .setFooter({ text: 'Not for real world use! Bot coded by DY and BY. ', iconURL: 'https://cdn.discordapp.com/icons/612373312051478662/7fad3012e32dd58264ea884473a2552e.webp?size=96' });
+        message.channel.send({ embeds: [error4Embed] });
+      }
+    
+  
+  
+  })
   }
   if (command == 'wheretofly'){
     const randomEmbed = new MessageEmbed()
-	  .setColor('#33FFC1')
-	  .setTitle('**Looking for Somewhere to Fly?**')
+	  .setColor('0x5f5e66')
+	  .setTitle('**Looking Somewhere to Fly?**')
     .setURL('https://flightaware.com/live/airport/random')
-    .setDescription('Click Here to View the Details and Diagram of the Random Aiport ↑')
-    .setTimestamp()
+    .setDescription('Click here to view the details and diagrams of the random airport ↑')
+    .setFooter({ text: 'Not for real world use! Bot coded by DY and BY', iconURL: 'https://cdn.discordapp.com/icons/612373312051478662/7fad3012e32dd58264ea884473a2552e.webp?size=96' });
 	  message.channel.send({ embeds: [randomEmbed] });
   }
     
-  if (metarSecond === 'metar'){
-    let getMetarr = async () => {
-        let response = await axios.get('https://api.aviationapi.com/v1/weather/metar?apt=' + metarFirst);
-        let metarrr = response.data;
-        return metarrr
-    
-    }
-
-    let getInfooo = async () => {
-      let response = await axios.get('https://www.airport-data.com/api/ap_info.json?icao='+ metarFirst);
-      let infooo = response.data;
-      return infooo;
-  }
-    let infoooValue = await getInfooo ();
-    let metar2Value = await getMetarr ()
-    let name = metarFirst.toUpperCase()
-    const metar2Embed = new MessageEmbed()
-	  .setColor('#33E6FF')
-	  .setTitle(`**METAR for** ${infoooValue.name} , ${infoooValue.location}  `)
-	  .setDescription('**ICAO:** ' + metar2Value[name].station_id + '\n**Temperature:** ' +  metar2Value[name].temp + 'C°'+ '\n**DewPoint:** ' +  metar2Value[name].dewpoint + ' C°' + '\n**Wind Direction:** '+  metar2Value[name].wind + '\n**Wind Velocity:** ' + metar2Value[name].wind_vel + ' Kts'+  '\n**Visibility:** ' +  metar2Value[name].visibility +  ' Miles' +  '\n**Altimeter:** ' + metar2Value[name].alt_hg +  '\n**Flight Conditions:** ' + metar2Value[name].category + '\n**Raw Metar:** ' +  metar2Value[name].raw + '\n**Observed:** ' + metar2Value[name].time_of_obs  )
-	  message.channel.send({ embeds: [metar2Embed] });
-
-  }
-
-  if (infoSecond === 'info'){
-    let getInfo = async () => {
-        let response = await axios.get('https://www.airport-data.com/api/ap_info.json?icao='+ infoFirst);
-        let infoo = response.data;
-        return infoo;
-    }
-    
-    let getMetar = async () => {
-          let response = await axios.get('https://api.checkwx.com/metar/' + metarFirst + '?x-api-key=ac348a7ad28f466aafd44dc34f');
-          let metarr = response.data;
-          return metarr;
-  
-    }
-    let infooValue = await getInfo ();
-    let metarValue = await getMetar ();
-    const infoEmbed = new MessageEmbed()
-	  .setColor('#9FFF33')
-	  .setTitle(`**Information for** ${infooValue.name} `)
-    .setURL('https://flightaware.com/resources/airport/' + firstPart.toUpperCase() + '/APD/AIRPORT+DIAGRAM/pdf')
-    .setDescription(`**ICAO**: ${infooValue.icao} \n**IATA**: ${infooValue.iata} \n**Name**: ${infooValue.name} \n**Location**: ${infooValue.location} \n**Country**: ${infooValue.country} \n**Metar**: ${metarValue.data}`)
-    .setImage('https://flightaware.com/resources/airport/' + firstPart.toUpperCase() + '/APD/AIRPORT+DIAGRAM/png')
-    .setTimestamp()
-	  message.channel.send({ embeds: [infoEmbed] });
-  
-
-}
 
 if (tecThird === 'tec'){
-  let getTec = async () => {
-    let response = await axios.get('https://api.aviationapi.com/v1/preferred-routes/search?origin='+ tecFirst + '&dest=' + tecSecond);
-    let Tecc = response.data;
-   return Tecc;
-  }
+  async function getTec () {
+    try {
+      let response = await axios.get('https://api.aviationapi.com/v1/preferred-routes/search?origin='+ tecFirst + '&dest=' + tecSecond)
+      let Tecc = response.data;
+      return Tecc;
+    
+    } catch (e) {
+      console.log(e.response)
+    }
+    }
   let TeccValue = await getTec ();
   TeccValue.map(getTeccc);
-
-
+  console.log(TeccValue)
   function getTeccc(item) {
     return ('\n**Origin:** ' + item.origin + '\n** Destination:**  ' + item.destination + '\n** Route:**  ' + item.route + '\n**Altitude:**  ' + item.altitude + '\n**Flow:**  ' + item.flow + '\n**Ops:**  ' + item.area.slice(-6) + '\n').toString()
   }
-  
+  if (TeccValue[0]?.d_artcc === 'ZLA') {
   const tecEmbed = new MessageEmbed()
-  .setColor('#5E33FF')
+  .setColor('0x5f5e66')
 	.setTitle(tecFirst.toUpperCase() + ' to ' + tecSecond.toUpperCase() + ' TEC Routes')
   .setDescription('' + TeccValue.map(getTeccc))
+  .setFooter({ text: 'Not for real world use! Bot coded by DY and BY. Routes provided by AviationAPI', iconURL: 'https://cdn.discordapp.com/icons/612373312051478662/7fad3012e32dd58264ea884473a2552e.webp?size=96' });
 	message.channel.send({ embeds: [tecEmbed] });
-  
-  
+}else{
+  const error3Embed = new MessageEmbed()
+  .setColor('0xff0000')
+  .setTitle('**Error!**')
+  .setDescription('No TEC route found.')
+  .setFooter({ text: 'Not for real world use! Bot coded by DY and BY. ', iconURL: 'https://cdn.discordapp.com/icons/612373312051478662/7fad3012e32dd58264ea884473a2552e.webp?size=96' });
+  message.channel.send({ embeds: [error3Embed] });
 }
-
-if (command == 'cmds'){
-  const cmdsEmbed = new MessageEmbed()
-	  .setColor('FCFF33')
-	  .setTitle('**Commands Available For Use**')
-    .setDescription('**-icaometar** Shows the Metar for the Aiport(Some Aiports are not Covered)\n**-icaoinfo** Shows the Information for the Aiport \n**-icaoapd** Shows the Airport Diagram for the Specified Aiport(Only Works with Aiports under FAA Jurisdiction) \n**-wheretofly** Shows a Random Aiport Where You Can Fly To \n**-faafaatec** Shows TEC routes between two airports in ZLA airspace.(Uses the FAA Codes) ex.-sanlaxtec\n**-stats[id]** Shows the statistics for the specific id \n**-tophours** Shows the current Controllers With the Most Hours ' )
-	  message.channel.send({ embeds: [cmdsEmbed] });
+  
   
 }
 if (command == 'help'){
   const helpEmbed = new MessageEmbed()
-	  .setColor('#FF333F')
-	  .setTitle('**Need Help?**')
-    .setDescription('If you would like to report a bug please **DM Mr. Nasa Man#7426** \nFor help about the ARTCC please **DM the DATM** \nFor any suggestions please **DM Mr. Nasa Man#7426** \nFor any other inquires talk to either the **ATM or DATM of ZLA** \nTo see all of the commands try -cmds' )
+	  .setColor('0x5f5e66')
+	  .setTitle('**Need Help?** \nBot Prefix = "!"')
+    .setDescription('**For bug reports, suggestions, or feedback please fill out this Google Form.**\nhttps://forms.gle/SC2JaXmnxYM1DvEe9\n**To view all of the commands try !cmds**\n**For general inquires please contact ZLA staff members.**')
+    .setFooter({ text: 'Not for real world use! Bot coded by DY and BY. Weather and airport information provided by AVWX. Digital ATIS provided by https://datis.clowd.io/. TEC routes provided by AviationAPI. IFR routes provided by FlightAware VATSIM stats provided by the VATSIM and VATUSA API.', iconURL: 'https://cdn.discordapp.com/icons/612373312051478662/7fad3012e32dd58264ea884473a2552e.webp?size=96' });
 	  message.author.send({ embeds: [helpEmbed] });
   
 }
 if (statsfirst === 'stats'){
-  let getStats= async () => {
-    let response = await axios.get('https://api.vatusa.net/v2/user/' + statsSecond);
-    let stat = response.data;
-    return stat;
-}
-  let getStats2= async () => {
+  async function getStats () {
+    try {
+      let response = await axios.get('https://api.vatusa.net/v2/user/' + statsSecond);
+      let stat = response.data;
+      return stat;
+    } catch (e) {
+      console.log(e)
+    }
+    }
+
+async function getStats2 () {
+try {
     let response = await axios.get('https://api.vatsim.net/api/ratings/' + statsSecond + '/rating_times/');
     let stat2 = response.data;
     return stat2;
+} catch (e) {
+  console.log(e)
+}
 }
   let statValue = await getStats();
   let stat2Value = await getStats2();
   let somet = 'data';
+  if (typeof stat2Value === 'undefined'){
+    const error2Embed = new MessageEmbed()
+    .setColor('0xff0000')
+	  .setTitle('**Error!**')
+    .setDescription('No user found. Please use a valid CID. You can find yours at https://my.vatsim.net/profile')
+    .setFooter({ text: 'Not for real world use! Bot coded by DY and BY. ', iconURL: 'https://cdn.discordapp.com/icons/612373312051478662/7fad3012e32dd58264ea884473a2552e.webp?size=96' });
+	  message.channel.send({ embeds: [error2Embed] });
+  } else {
   const Stats2Embed = new MessageEmbed()
-  .setColor('#33E6FF')
+  .setColor('0x5f5e66')
   .setTitle('Statistics for ' + statValue[somet].fname + ' ' + statValue[somet].lname)
-  .setDescription('**Facility: ** '+  statValue[somet].facility + '\n**Rating: ** ' + statValue[somet].rating_short  + '\n**Mentor: ** ' + statValue[somet].isMentor + '\n**--------Overall Times--------**' + '\n**Overall ATC: **' +  stat2Value.atc + '\n**Overall Pilot: **' + stat2Value.pilot + '\n**S1: **' +  stat2Value.s1 + '\n**S2: **' +  stat2Value.s2 + '\n**S3: **' +  stat2Value.s3 + '\n**C1: **' +  stat2Value.c1 + '\n**C3: **' +  stat2Value.c3 + '\n**I1: **' +  stat2Value.i1 + '\n**---------------------------------**' + '\n**Joined: ** ' + statValue[somet].created_at  )
+  .setDescription('**Facility: ** '+  statValue[somet].facility + '\n**Rating: ** ' + statValue[somet].rating_short  + '\n**--------Overall Times--------**' + '\n**Overall ATC: **' +  stat2Value.atc + '\n**Overall Pilot: **' + stat2Value.pilot + '\n**S1: **' +  stat2Value.s1 + '\n**S2: **' +  stat2Value.s2 + '\n**S3: **' +  stat2Value.s3 + '\n**C1: **' +  stat2Value.c1 + '\n**C3: **' +  stat2Value.c3 + '\n**I1: **' +  stat2Value.i1 + '\n**---------------------------------**' + '\n**Joined: ** ' + statValue[somet].created_at  )
+  .setFooter({ text: 'Not for real world use! Bot coded by DY and BY. VATSIM stats provided by the VATUSA and VATSIM API.', iconURL: 'https://cdn.discordapp.com/icons/612373312051478662/7fad3012e32dd58264ea884473a2552e.webp?size=96' });
   message.channel.send({ embeds: [Stats2Embed] });
-
+}
 }
 
+
 if (topten == 'tophours'){
+  toptenText = []
   puppeteer
   .launch()
   .then(browser => browser.newPage())
@@ -280,30 +310,69 @@ if (topten == 'tophours'){
       });
     });
     const toptenEmbed = new MessageEmbed()
-	  .setColor('#11A655')
+	  .setColor('0x5f5e66')
 	  .setTitle('**Controllers With the Most Hours Currently**')
     .setDescription(`**--------Local--------**\n**1st:** ${toptenText[0].Localfirst} ${toptenText[1].LocalTime} \n**2nd:** ${toptenText[2].Localsecond} ${toptenText[3].LocalSecondTime} \n**3rd:** ${toptenText[4].Localthird} ${toptenText[5].LocalThirdTime}  \n**--------Tracon-------**\n**1st:** ${toptenText[6].Traconfirst} ${toptenText[7].TraconTime} \n**2nd:** ${toptenText[8].Traconsecond} ${toptenText[9].TraconsecondTime} \n**3rd:** ${toptenText[10].Traconthird} ${toptenText[11].TraconThirdTime} \n**--------Enroute------**\n**1st:** ${toptenText[12].Enroutefirst} ${toptenText[13].EnrouteTime} \n**2nd:** ${toptenText[14].Enroutesecond} ${toptenText[15].EnrouteSecondTime} \n**3rd:** ${toptenText[16].Enroutethird} ${toptenText[17].EnrouteThirdTime} `)
+    .setFooter({ text: 'Not for real world use! Bot coded by DY and BY.', iconURL: 'https://cdn.discordapp.com/icons/612373312051478662/7fad3012e32dd58264ea884473a2552e.webp?size=96' });
 	  message.channel.send({ embeds: [toptenEmbed] });
   })
   .catch(console.error);
-
-  
-  
+}
+if (online1 == 'online'){
+  eventText = []
+  puppeteer
+  .launch()
+  .then(browser => browser.newPage())
+  .then(page => {
+    return page.goto(url2).then(function() {
+      return page.content();
+    });
+  })
+  .then(html => {
+    const $ = cheerio.load(html);
+    $('#onlinedata > div:nth-child(2)').each(function() {
+      eventText.push({
+        eventLink: $(this).text(),
+      });
+    });
     
   
+    const eventsEmbed = new MessageEmbed()
+	  .setColor('0x5f5e66')
+	  .setTitle('**Online Controllers**')
+    .setDescription(`${eventText[0].eventLink}`)
+    .setFooter({ text: 'Not for real world use! Bot coded by DY and BY.', iconURL: 'https://cdn.discordapp.com/icons/612373312051478662/7fad3012e32dd58264ea884473a2552e.webp?size=96' });
+	  message.channel.send({ embeds: [eventsEmbed] });
   
-}
+
+  });
+  
+
+  }
+  if (traffic1 === 'traffic'){
+    vatsim_stats.apt_data.getAllAiportData(traffic2).then((response) => {
+    var list = ['KLAS',"KLAX",'KLSV','KNKX','KSAN','KBUR','KPSP','KRIV','KSBA','KSNA','KBFL',	'KCMA','KCNO','KCRQ','KEDW','KEMT','KFUL','KGCN','KHHR',	'KHND','KIFP','KINS',	'KLGB',		'KLGF',	'KMHV',	'KMYF',	'KNFG',	'KNID',	'KNJK','KNRS','KNSI','KNTD'	,'KNUC',	'KNXP',	'KNYL',	'KNZY',	'KOXR',	'KPMD',	'KPOC','KRAL',	'KRNM',	'KSBD', 'KSBP',	'KSDM',	'KSEE',	'KSLI',	'KSMO',	'KSMX',		'KTOA','KVBG',	'KVCV', 'KVGT','KVNY','KWHP','KWJF',	'KBLH',	'KEED',	'KIPL',	'KLPC',		'KSGU',		'KTRM',	];
+    if(list.includes(traffic2.toUpperCase())){
+
+    const trafEmbed = new MessageEmbed()
+	  .setColor('0x5f5e66')
+	  .setTitle('**Departures, Arrivals, and Controllers at **'+ traffic2.toUpperCase())
+    .setDescription('**Departures: **' + response.departures + '\n**Arrivals: **' + response.arrivals + '\n**Controllers: **' + response.controllers )
+    .setFooter({ text: 'Not for real world use! Bot coded by DY and BY.', iconURL: 'https://cdn.discordapp.com/icons/612373312051478662/7fad3012e32dd58264ea884473a2552e.webp?size=96' });
+	  message.channel.send({ embeds: [trafEmbed] });
+    }else{
+      const error5Embed = new MessageEmbed()
+      .setColor('0xff0000')
+      .setTitle('**Error!**')
+      .setDescription('ICAO code is invalid or is outside the ZLA airspace. Please input one to meet the criteria.')
+      . setFooter({ text: 'Not for real world use! Bot coded by DY and BY. ', iconURL: 'https://cdn.discordapp.com/icons/612373312051478662/7fad3012e32dd58264ea884473a2552e.webp?size=96' });
+      message.channel.send({ embeds: [error5Embed] });
+    }
+  })
+      }
+
+  
+
 });
 
-
-client.login('OTk0NjYzNzA1ODM5MTQ5MDc2.GQ_73d.62jbgWt7aULnds1ukNEayCaON3DlkNzAKlpZ5g');
-
-
-
-
-
-
-
-
-
-
+client.login('');
