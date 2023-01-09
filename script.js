e5c1ab4da000a81dd05e3d9d9940eb371307d385
MrNasaMan <109177113+MrNasaMan@users.noreply.github.com>
2023-01-09 15:37:02 -0800

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ var toptenText = [];
 var eventText = [];
 const url2 = 'https://laartcc.org';
 const vatsim_stats = require('vatsim-stats');
+const dateText = []
+const {handler} = require('vatsim-data-handler');
+const request = require('request')
 
 
 
@@ -20,19 +23,103 @@ const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
 const prefix = '!';
 
-const token = '';
+const token = 'token here';
+
+
 
 
 client.once('ready', () => {
-  console.log('The ZLA Bot has Booted up the Scope!')
-
   
 
+  setInterval(function() {
+    axios.get('https://www.timeapi.io/api/Time/current/zone?timeZone=US/Pacific')
+    .then(function (response) {
+    if (response.data.date.slice(3,5) == '28'){ 
+    const url4 = 'https://laartcc.org/topten';
+    puppeteer
+    .launch()
+    .then(browser => browser.newPage())
+    .then(page => {
+    return page.goto(url4, {waitUntil: 'load', timeout: 0}).then(function() {
+      return page.content();
+    });
+  })
+  .then(html => {
+    const $ = cheerio.load(html);
+    
+    $('body > div.container.pt-2 > div:nth-child(2) > div.col-md-9 > div:nth-child(3) > div.card.border-danger > ul > li:nth-child(1) > a').each(function() {
+      dateText.push({
+      LocalWinner: $(this).text(),
+    });
+    });
+    $('body > div.container.pt-2 > div:nth-child(2) > div.col-md-9 > div:nth-child(3) > div.card.border-danger > ul > li:nth-child(1) > span.float-right').each(function() {
+      dateText.push({
+      LocalTimer: $(this).text(),
+      });
+    });
+      
+    $('body > div.container.pt-2 > div:nth-child(2) > div.col-md-9 > div:nth-child(3) > div.card.border-success > ul > li:nth-child(1) > a').each(function() {
+      dateText.push({
+      TraconWinner: $(this).text(),
+      });
+    });
+    $('body > div.container.pt-2 > div:nth-child(2) > div.col-md-9 > div:nth-child(3) > div.card.border-success > ul > li:nth-child(1) > span.float-right').each(function() {
+      dateText.push({
+      TraconTimer: $(this).text(),
+      });
+    });
+    $('body > div.container.pt-2 > div:nth-child(2) > div.col-md-9 > div:nth-child(3) > div.card.border-primary > ul > li:nth-child(1) > a').each(function() {
+     dateText.push({
+     EnrouteWinner: $(this).text(),
+     });
+    });
+    $('body > div.container.pt-2 > div:nth-child(2) > div.col-md-9 > div:nth-child(3) > div.card.border-primary > ul > li:nth-child(1) > span.float-right').each(function() {
+      dateText.push({
+      EnrouteTimer: $(this).text(),
+      });
+    });
+    const miniu = dateText[1].LocalTimer.slice(3,5)
+    const miniu2 = dateText[3].TraconTimer.slice(3,5)
+    const miniu3 = dateText[5].EnrouteTimer.slice(3,5)
+    const timer1 = 'minutes'
+    const timer2 = 'minutes'
+    const timer3 = 'minutes'
+    if (miniu == '01'){
+      timer1 = 'minute'
+    }else if (miniu == '00'){
+      timer1 = ' '
+    }
+    if (miniu2 == '01'){
+      timer2 = 'minute'
+    }else if (miniu2 == '00'){
+      timer2 = ' '
+    }
+    if (miniu3 == '01'){
+      timer3 = 'minute'
+    }else if (miniu3 == '00'){
+      timer3 = ' '
+    }
+    const tophurEmbed = new MessageEmbed()
+    .setColor('0x5f5e66')
+    .setTitle('**Top Controllers of the Month**')
+    .setDescription('Good afternoon aviatiors, every month we look at and find the controller with the most hours in three categories, local, tracon, and enroute.The winners for this month are the following:\n\n**Local:**\n'+'`'+`${dateText[0].LocalWinner}`+'`'+ ' with a time of ' +'`' +`${dateText[1].LocalTimer.slice(0,2)}`+' hours'+'`'+  ' and ' + '`'+`${dateText[1].LocalTimer.slice(3,5)}` + ' '+ timer1+'`'+ '\n**Tracon:**\n' + '`'+`${dateText[2].TraconWinner}`+'`'+ ' with a time of ' +'`' +`${dateText[3].TraconTimer.slice(0,2)}`+' hours'+'`' +' and ' + '`' +`${dateText[3].TraconTimer.slice(3,5)}` +' '+timer2+'`'+ '\n**Enroute:**\n' + '`'+`${dateText[4].EnrouteWinner}`+'`'+ ' with a time of ' +'`' +`${dateText[5].EnrouteTimer.slice(0,2)}`+' hours'+'`' +' and ' + '`' +`${dateText[5].EnrouteTimer.slice(3,5)}`+' '+timer3+'`'+ '\n\nCongratulations to all three controllers for winning this months top controllers list. Special thanks to all the controllers who provided ATC coverage this month in ZLA!')
+    .setFooter({ text: 'Winners are announced on the 28th of each month. Bot coded by DY and BY ', iconURL: 'https://cdn.discordapp.com/icons/612373312051478662/7fad3012e32dd58264ea884473a2552e.webp?size=96' });
+    client.channels.cache.get("998980240401502328").send({ embeds: [tophurEmbed] });
+    })
+      
+    }else{
+      console.log('not today')
+    }
+    
+  })
+    }, 86400000);
+  
 });
 
 
 
 client.on('message', async message =>{
+
   if(!message.content.startsWith(prefix) || message.author.bot) return;
 
   const args = message.content.slice(prefix.length).split(/ +/);
@@ -42,15 +129,14 @@ client.on('message', async message =>{
   const tecFirst = command.slice(0,3);
   const tecSecond = command.slice(3, 6);
   const tecThird = command.slice(6, 9);
-  const statsSecond = command.slice(5, 13);
-  const statsfirst = command.slice(0, 5);
   const topten = command.slice(0, 8);
-  const online1 = command.slice(0, 6);
   const analyzer1 = command.slice(8, 16);
   const analyzer2 = command.slice(0, 4);
   const analyzer3 = command.slice(4, 8);
-  const traffic1 = command.slice(0,7);
-  const traffic2 = command.slice(7,11);
+ 
+
+  
+
  
 
   
@@ -58,11 +144,12 @@ client.on('message', async message =>{
 
   if (analyzer1 === 'ifrroute'){
     const url3 = 'https://flightaware.com/analysis/route.rvt?origin=' + analyzer2.toUpperCase() + '&destination=' + analyzer3.toUpperCase()
+    
     puppeteer
     .launch()
     .then(browser => browser.newPage())
     .then(page => {
-    return page.goto(url3).then(function() {
+    return page.goto(url3, {waitUntil: 'load', timeout: 0}).then(function() {
       return page.content();
     });
   })
@@ -93,7 +180,7 @@ client.on('message', async message =>{
       const analyzerEmbed = new MessageEmbed()
       .setColor('0x5f5e66')
       .setTitle(analyzer2.toUpperCase() + ' to ' + analyzer3.toUpperCase() + ' IFR routes in terms of popularity')
-      .setDescription(text2.toString())
+      .setDescription('`'+text2.toString() +'`')
       .setFooter({ text: 'Not for real world use! Bot coded by DY and BY. Routes provided by FlightAware', iconURL: 'https://cdn.discordapp.com/icons/612373312051478662/7fad3012e32dd58264ea884473a2552e.webp?size=96' });
       if (text2[0] == '0') {
         message.channel.send({ embeds: [analyzerEmbed] });
@@ -156,7 +243,8 @@ if (tecThird === 'tec'){
   
   
 }
-if (command == 'help'){
+if (message.content.startsWith('!help')){
+  
   const helpEmbed = new MessageEmbed()
 	  .setColor('0x5f5e66')
 	  .setTitle('**Need Help?** \nBot Prefix = "!"')
@@ -165,7 +253,8 @@ if (command == 'help'){
 	  message.author.send({ embeds: [helpEmbed] });
   
 }
-if (statsfirst === 'stats'){
+if (message.content.startsWith('!stats')) {
+  const statsSecond = message.content.split(' ')[1];
   async function getStats () {
     try {
       let response = await axios.get('https://api.vatusa.net/v2/user/' + statsSecond);
@@ -198,8 +287,8 @@ try {
   } else {
   const Stats2Embed = new MessageEmbed()
   .setColor('0x5f5e66')
-  .setTitle('Statistics for ' + statValue[somet].fname + ' ' + statValue[somet].lname)
-  .setDescription('**Facility: ** '+  statValue[somet].facility + '\n**Rating: ** ' + statValue[somet].rating_short  + '\n**--------Overall Times--------**' + '\n**Overall ATC: **' +  stat2Value.atc + '\n**Overall Pilot: **' + stat2Value.pilot + '\n**S1: **' +  stat2Value.s1 + '\n**S2: **' +  stat2Value.s2 + '\n**S3: **' +  stat2Value.s3 + '\n**C1: **' +  stat2Value.c1 + '\n**C3: **' +  stat2Value.c3 + '\n**I1: **' +  stat2Value.i1 + '\n**---------------------------------**' + '\n**Joined: ** ' + statValue[somet].created_at  )
+  .setTitle('Statistics for ' + '`' + statValue[somet].fname + ' ' + statValue[somet].lname+'`')
+  .setDescription('**Facility: ** '+  '`' + statValue[somet].facility+'`' + '\n**Rating: ** ' + '`' + statValue[somet].rating_short+'`'  + '\n**--------Overall Times--------**' + '\n**Overall ATC: **' +  '`' + stat2Value.atc+'`' + '\n**Overall Pilot: **' + '`' + stat2Value.pilot+'`' + '\n**S1: **' +  '`' + stat2Value.s1+'`' + '\n**S2: **' +  '`' + stat2Value.s2+'`' + '\n**S3: **' +  '`' + stat2Value.s3+'`' + '\n**C1: **' +  '`' + stat2Value.c1+'`' + '\n**C3: **' +  '`' + stat2Value.c3+'`' + '\n**I1: **' +  '`' + stat2Value.i1+'`' + '\n**---------------------------------**' + '\n**Joined: ** ' + '`' + statValue[somet].created_at+'`'  )
   .setFooter({ text: 'Not for real world use! Bot coded by DY and BY. VATSIM stats provided by the VATUSA and VATSIM API.', iconURL: 'https://cdn.discordapp.com/icons/612373312051478662/7fad3012e32dd58264ea884473a2552e.webp?size=96' });
   message.channel.send({ embeds: [Stats2Embed] });
 }
@@ -212,7 +301,7 @@ if (topten == 'tophours'){
   .launch()
   .then(browser => browser.newPage())
   .then(page => {
-    return page.goto(url).then(function() {
+    return page.goto(url, {waitUntil: 'load', timeout: 0}).then(function() {
       return page.content();
     });
   })
@@ -310,53 +399,24 @@ if (topten == 'tophours'){
     });
     const toptenEmbed = new MessageEmbed()
 	  .setColor('0x5f5e66')
-	  .setTitle('**Controllers With the Most Hours Currently**')
-    .setDescription(`**--------Local--------**\n**1st:** ${toptenText[0].Localfirst} ${toptenText[1].LocalTime} \n**2nd:** ${toptenText[2].Localsecond} ${toptenText[3].LocalSecondTime} \n**3rd:** ${toptenText[4].Localthird} ${toptenText[5].LocalThirdTime}  \n**--------Tracon-------**\n**1st:** ${toptenText[6].Traconfirst} ${toptenText[7].TraconTime} \n**2nd:** ${toptenText[8].Traconsecond} ${toptenText[9].TraconsecondTime} \n**3rd:** ${toptenText[10].Traconthird} ${toptenText[11].TraconThirdTime} \n**--------Enroute------**\n**1st:** ${toptenText[12].Enroutefirst} ${toptenText[13].EnrouteTime} \n**2nd:** ${toptenText[14].Enroutesecond} ${toptenText[15].EnrouteSecondTime} \n**3rd:** ${toptenText[16].Enroutethird} ${toptenText[17].EnrouteThirdTime} `)
+	  .setTitle('**Top Three Controllers**')
+    .setDescription(`**--------Local--------**\n**1st:**`+ `${toptenText[0].Localfirst}`+' `'+ `${toptenText[1].LocalTime}` + '`'+  `\n**2nd:**` + `${toptenText[2].Localsecond}`+ ' `'+ `${toptenText[3].LocalSecondTime}` + '`' + `\n**3rd:**`  + `${toptenText[4].Localthird}` + '`' + `${toptenText[5].LocalThirdTime}` +'`' +  `\n**--------Tracon-------**\n**1st:**`+ `${toptenText[6].Traconfirst}`+'`'+ `${toptenText[7].TraconTime}`+'`'+ `\n**2nd:**`+ `${toptenText[8].Traconsecond}`+'`'+ `${toptenText[9].TraconsecondTime}`+'`'+ `\n**3rd:**`+ `${toptenText[10].Traconthird}`+'`'+ `${toptenText[11].TraconThirdTime}`+'`' +`\n**--------Enroute------**\n**1st:**` + `${toptenText[12].Enroutefirst}`+'`'+`${toptenText[13].EnrouteTime}` + '`'+ `\n**2nd:**` + `${toptenText[14].Enroutesecond}`+'`'+ `${toptenText[15].EnrouteSecondTime}` +'`'+ `\n**3rd:**` +  `${toptenText[16].Enroutethird}` + '`' +`${toptenText[17].EnrouteThirdTime}`+'`')
     .setFooter({ text: 'Not for real world use! Bot coded by DY and BY.', iconURL: 'https://cdn.discordapp.com/icons/612373312051478662/7fad3012e32dd58264ea884473a2552e.webp?size=96' });
 	  message.channel.send({ embeds: [toptenEmbed] });
   })
   .catch(console.error);
 }
-if (online1 == 'online'){
-  eventText = []
-  puppeteer
-  .launch()
-  .then(browser => browser.newPage())
-  .then(page => {
-    return page.goto(url2).then(function() {
-      return page.content();
-    });
-  })
-  .then(html => {
-    const $ = cheerio.load(html);
-    $('#onlinedata > div:nth-child(2)').each(function() {
-      eventText.push({
-        eventLink: $(this).text(),
-      });
-    });
-    
-  
-    const eventsEmbed = new MessageEmbed()
-	  .setColor('0x5f5e66')
-	  .setTitle('**Online Controllers**')
-    .setDescription(`${eventText[0].eventLink}`)
-    .setFooter({ text: 'Not for real world use! Bot coded by DY and BY.', iconURL: 'https://cdn.discordapp.com/icons/612373312051478662/7fad3012e32dd58264ea884473a2552e.webp?size=96' });
-	  message.channel.send({ embeds: [eventsEmbed] });
-  
 
-  });
-  
-
-  }
-  if (traffic1 === 'traffic'){
+  if (message.content.startsWith('!traffic')){
+    const traffic2 =  message.content.split(' ')[1];
     vatsim_stats.apt_data.getAllAiportData(traffic2).then((response) => {
     var list = ['KLAS',"KLAX",'KLSV','KNKX','KSAN','KBUR','KPSP','KRIV','KSBA','KSNA','KBFL',	'KCMA','KCNO','KCRQ','KEDW','KEMT','KFUL','KGCN','KHHR',	'KHND','KIFP','KINS',	'KLGB',		'KLGF',	'KMHV',	'KMYF',	'KNFG',	'KNID',	'KNJK','KNRS','KNSI','KNTD'	,'KNUC',	'KNXP',	'KNYL',	'KNZY',	'KOXR',	'KPMD',	'KPOC','KRAL',	'KRNM',	'KSBD', 'KSBP',	'KSDM',	'KSEE',	'KSLI',	'KSMO',	'KSMX',		'KTOA','KVBG',	'KVCV', 'KVGT','KVNY','KWHP','KWJF',	'KBLH',	'KEED',	'KIPL',	'KLPC',		'KSGU',		'KTRM',	];
     if(list.includes(traffic2.toUpperCase())){
 
     const trafEmbed = new MessageEmbed()
 	  .setColor('0x5f5e66')
-	  .setTitle('**Departures, Arrivals, and Controllers at **'+ traffic2.toUpperCase())
-    .setDescription('**Departures: **' + response.departures + '\n**Arrivals: **' + response.arrivals + '\n**Controllers: **' + response.controllers )
+	  .setTitle('**Departures, Arrivals, and Controllers at **'+ '`' +traffic2.toUpperCase()+'`')
+    .setDescription('**Departures: **' + '`' + response.departures+'`' + '\n**Arrivals: **' + '`'+response.arrivals+'`' + '\n**Controllers: **' + '`'+response.controllers+'`' )
     .setFooter({ text: 'Not for real world use! Bot coded by DY and BY.', iconURL: 'https://cdn.discordapp.com/icons/612373312051478662/7fad3012e32dd58264ea884473a2552e.webp?size=96' });
 	  message.channel.send({ embeds: [trafEmbed] });
     }else{
@@ -370,8 +430,42 @@ if (online1 == 'online'){
   })
       }
 
+    if (message.content.startsWith('!popular')){
+    
+    handler.getPopularAirports().then(val => {
+      handler.getCount('controllers').then(val1 => {
+        handler.getCount('pilots').then(val2 => {
+          handler.getCount('all').then(val3 => {
+            const popEmbed = new MessageEmbed()
+            .setColor('0x5f5e66')
+            .setTitle('Most Popular Airports on VATSIM')
+            .setDescription('**1: **' + '`'+ val[0].id +'`' + '\n> **Aircraft: **' + val[0].count+ '\n**2: **' + '``'+ val[1].id +'``'+ '\n> **Aircraft: **' + val[1].count+ '\n**3: **' + '`'+ val[2].id +'`'+ '\n> **Aircraft: **' + val[2].count+ '\n**4: **' + '`'+ val[3].id +'`'+  '\n> **Aircraft: **' + val[3].count+'\n**5: **' + '`'+ val[4].id +'`'+  '\n> **Aircraft: **' + val[4].count+'\n**6: **' + '`'+ val[5].id +'`'+ '\n> **Aircraft: **' + val[5].count+ '\n**7: **' + '`'+ val[6].id +'`'+  '\n> **Aircraft: **' + val[6].count+'\n**8: **' + '`'+ val[7].id +'`'+ '\n> **Aircraft: **' + val[7].count+ '\n**9: **' + '`'+ val[8].id +'`'+  '\n> **Aircraft: **' + val[8].count+'\n**10: **' + '`'+ val[9].id +'`'+ '\n> **Aircraft: **' + val[9].count)
+            .addFields(
+              { name: 'Controllers', value: ' ' + val1, inline: true },
+              { name: 'Pilots', value: ' ' + val2, inline: true },
+              { name: 'Total', value: ' ' + val3, inline: true },
+            )
+            .setFooter({ text: 'VATSIM data by hcphoon01. Not for real world use! Bot coded by DY and BY.', iconURL: 'https://cdn.discordapp.com/icons/612373312051478662/7fad3012e32dd58264ea884473a2552e.webp?size=96' });
+            message.channel.send({ embeds: [popEmbed] })
+          })
+        })
+      
+      })
+      
+    })
+  
+  }
+
+  
+   
+   
+  
   
 
 });
 
-client.login('');
+client.login(token);
+
+
+
+
